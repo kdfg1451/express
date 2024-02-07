2da6d0615c28bdfbb7bf1df6ebc97dbb4a83d627
kdfg1451 <2001015103wd@cyber-u.ac.jp>
2024-02-07 21:38:25 +0900

    add api yesno

--- app.js
+++ app.js
@@ -8,6 +8,9 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var helloRouter =require('./routes/hello');
 var notesRouter =require('./routes/notes');
+var catRouter =require('./routes/cat');
+var yesnoRouter =require('./routes/yesno');
+//var qrcodeRouter =require('./routes/qrcode');
 
 var app = express();
 
@@ -25,6 +28,9 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/hello', helloRouter);
 app.use('/notes', notesRouter);
+app.use('/cat', catRouter);
+app.use('/yesno', yesnoRouter);
+//app.use('/qrcode', qrcodeRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
